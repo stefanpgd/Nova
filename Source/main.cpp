1d72982501f6bf54e5c6bbce0a737d6a5e181ffd
Stefan Pijnacker <stefanpijnacker@gmail.com>
2023-05-29 16:58:08 +0200

    Added required libraries. Factory, Adapter and Device creation 💽

--- Source/main.cpp
+++ Source/main.cpp
@@ -17,7 +17,7 @@ using namespace Microsoft::WRL;
 #include <DirectXMath.h>
 
 // D3D12 extension library.
-#include <d3dx12.h>
+#include "d3dx12.h"
 
 // STL Headers
 #include <iostream>
@@ -25,17 +25,7 @@ using namespace Microsoft::WRL;
 #include <cassert>
 #include <chrono>
 
-// From DXSampleHelper.h
-inline void ThrowIfFailed(HRESULT hr)
-{
-    if (FAILED(hr))
-    {
-        throw std::exception();
-    }
-}
-
 bool g_IsInitialized = false;
-bool g_UseWarp = false;
 const unsigned int g_NumFrames = 3; // amount of back buffers for swap chain
 
 unsigned int g_WindowWidth = 1280;
@@ -54,8 +44,133 @@ ComPtr<ID3D12DescriptorHeap> g_RTVDescriptorHeap;
 UINT g_RTVDescriptorSize;
 UINT g_CurrentBackBufferIndex;
 
+// Synchronization
+ComPtr<ID3D12Fence> g_Fence;
+unsigned int g_FenceValue = 0;
+unsigned int g_FrameFenceValues[g_NumFrames] = {};
+HANDLE g_FenceEvent;
+
+bool g_Vsync = true; // present frames at the same interval as the screen's refresh rate
+bool g_TearingSupported = false;
+
+bool g_Fullscreen = false;
+
+// Window message callback
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+// From DXSampleHelper.h
+inline void ThrowIfFailed(HRESULT hr)
+{
+	if (FAILED(hr))
+	{
+		throw std::exception();
+	}
+}
+
+void EnableDebugLayer()
+{
+#if defined (_DEBUG)
+	ComPtr<ID3D12Debug> debugInterface;
+	ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(&debugInterface)));
+	debugInterface->EnableDebugLayer();
+#endif
+}
+
+void RegisterWindowClass(HINSTANCE hInst, const wchar_t* windowClassName)
+{
+	WNDCLASSEXW windowClassDescription = {};
+
+	windowClassDescription.cbSize = sizeof(WNDCLASSEX);
+	windowClassDescription.style = CS_HREDRAW | CS_VREDRAW;
+	windowClassDescription.lpfnWndProc = &WndProc;
+	windowClassDescription.cbClsExtra = 0;
+	windowClassDescription.cbWndExtra = 0;
+	windowClassDescription.hInstance = hInst;
+	windowClassDescription.hIcon = ::LoadIcon(hInst, NULL);
+	windowClassDescription.hCursor = ::LoadCursor(NULL, IDC_ARROW);
+	windowClassDescription.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	windowClassDescription.lpszMenuName = NULL;
+	windowClassDescription.lpszClassName = windowClassName;
+	windowClassDescription.hIconSm = ::LoadIcon(hInst, NULL);
+
+	static ATOM atom = ::RegisterClassExW(&windowClassDescription);
+	assert(atom > 0);
+}
+
+HWND CreateWindow(const wchar_t* windowClassName, HINSTANCE hInst, const wchar_t* windowName,
+	unsigned int width, unsigned int height)
+{
+	int screenWidth = ::GetSystemMetrics(SM_CXSCREEN);
+	int screenHeight = ::GetSystemMetrics(SM_CYSCREEN);
+
+	RECT windowRect = { 0, 0, static_cast<LONG>(width), static_cast<LONG>(height) };
+	::AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+	int windowWidth = windowRect.right - windowRect.left;
+	int windowHeight = windowRect.bottom - windowRect.top;
+
+	// Center the window within the screen. Clamp to 0, 0 for the top-left corner.
+	int windowX = std::max<int>(0, (screenWidth - windowWidth) / 2);
+	int windowY = std::max<int>(0, (screenHeight - windowHeight) / 2);
+
+	HWND hWnd = ::CreateWindowExW(NULL, windowClassName, windowName, WS_OVERLAPPEDWINDOW,
+		windowX, windowY, windowWidth, windowHeight, NULL, NULL, hInst, nullptr);
+
+	assert(hWnd && "Failed to create window");
+	return hWnd;
+}
+
+ComPtr<IDXGIAdapter4> GetAdapter(bool useWarp)
+{
+	ComPtr<IDXGIFactory4> dxgiFactory;
+	UINT createFactoryFlags = 0;
+#if defined(_DEBUG)
+	// Enables errors to be caught during device creation and while quering adapaters
+	createFactoryFlags = DXGI_CREATE_FACTORY_DEBUG;
+#endif
+
+	ThrowIfFailed(CreateDXGIFactory2(createFactoryFlags, IID_PPV_ARGS(&dxgiFactory)));
+
+	ComPtr<IDXGIAdapter1> dxgiAdapter1;
+	ComPtr<IDXGIAdapter4> dxgiAdapter4;
+
+	SIZE_T maxDedicatedVideoMemory = 0;
+	for (UINT i = 0; dxgiFactory->EnumAdapters1(i, &dxgiAdapter1) != DXGI_ERROR_NOT_FOUND; ++i)
+	{
+		DXGI_ADAPTER_DESC1 dxgiAdapterDesc1;
+		dxgiAdapter1->GetDesc1(&dxgiAdapterDesc1);
+
+		// Is adapter not a software GPU? Can it succesfully create a Dx12 device?
+		// Does it have more dedicated video memory than the other adapters? If so, store it as the most capable adapter
+		if ((dxgiAdapterDesc1.Flags & DXGI_ADAPTER_FLAG_SOFTWARE) == 0 &&
+			SUCCEEDED(D3D12CreateDevice(dxgiAdapter1.Get(),
+				D3D_FEATURE_LEVEL_11_0, __uuidof(ID3D12Device), nullptr)) &&
+			dxgiAdapterDesc1.DedicatedVideoMemory > maxDedicatedVideoMemory)
+		{
+			maxDedicatedVideoMemory = dxgiAdapterDesc1.DedicatedVideoMemory;
+			ThrowIfFailed(dxgiAdapter1.As(&dxgiAdapter4));
+		}
+	}
+
+	return dxgiAdapter4;
+}
+
+ComPtr<ID3D12Device2> CreateDevice(ComPtr<IDXGIAdapter4> adapter)
+{
+	ComPtr<ID3D12Device2> device;
+	ThrowIfFailed(D3D12CreateDevice(adapter.Get(), D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&device)));
+	return device;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT u, WPARAM w, LPARAM l)
+{
+	return NULL;
+}
+
 int main()
 {
 	printf("Hello World\n");
+	float x;
+	std::cin >> x;
 	return 0;
 }
